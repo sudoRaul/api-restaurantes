2797690bac661a71d704c341b1447b3131ad933d
sudoRaul <rrodfer05@gmail.com>
2025-07-25 09:31:14 +0200

    Simple CRUD hecho

--- app/Http/Controllers/RestauranteController.php
+++ app/Http/Controllers/RestauranteController.php
@@ -2,47 +2,108 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Restaurante;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class RestauranteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
+    // Devolvemos todos los restaurantes
     public function index()
     {
-        //
+        $restaurantes = Restaurante::all();
+
+        // Devolvemos una respuesta correcta siempre ya que la petición se ha realizado correctamente
+        return response()->json([
+            'success' => true,
+            'data' => $restaurantes->isEmpty() ? 'No se han encontrado restaurantes' : $restaurantes ,
+            'message' => 'Lista de restaurantes'
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    // Almacenamos un restaurante
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'nombre' => 'required|string|max:255',
+            'direccion' => 'required|string|max:255',
+            'telefono' => 'required|string|max:10',
+        ]);
+
+        $restaurante = Restaurante::create($validatedData);
+
+        return response()->json([
+            'success' => true,
+            'data' => $restaurante,
+            'message' => 'Restaurante creado correctamente'
+        ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
+
+    // Mostramos un único restaurante por id
     public function show(string $id)
     {
-        //
+        $restaurante = Restaurante::find($id);
+
+        if (!$restaurante) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Restaurante no encontrado'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $restaurante,
+            'message' => 'Restaurante encontrado'
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    // Actualizamos un restaurante por id
     public function update(Request $request, string $id)
     {
-        //
+        try{
+            $restaurante = Restaurante::findOrFail($id);
+
+            $validatedData = $request->validate([
+                'nombre' => 'sometimes|string|max:255',
+                'direccion' => 'sometimes|string|max:255',
+                'telefono' => 'sometimes|string|max:10',
+            ]);
+
+            $restaurante->update($validatedData);
+
+            return response()->json([
+                'success' => true,
+                'data' => $restaurante,
+                'message' => 'Restaurante actualizado correctamente'
+            ]);
+        }catch (ModelNotFoundException $e){
+            return response()->json([
+                'success' => false,
+                'message' => 'Restaurante no encontrado'
+            ], 404);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
+    // Eliminamos un restaurante por id
     public function destroy(string $id)
     {
-        //
+        try {
+            $restaurante = Restaurante::findOrFail($id);
+            $restaurante->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Restaurante eliminado correctamente'
+            ], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Restaurante no encontrado'
+            ], 404);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\RestauranteController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestauranteController;
 
 Route::apiResource('restaurantes', RestauranteController::class);
